616fef91f31ae2577a6cdc3be85a206f48cd83c7
AkshayPatil <akshaypatil781997@gmail.com>
2023-02-11 21:44:52 +0530

    lombok added

--- src/main/java/com/ap/jpa/model/Student.java
+++ src/main/java/com/ap/jpa/model/Student.java
@@ -4,8 +4,15 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
 @Table(name="student_details")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
 	
 	@Id
@@ -14,48 +21,5 @@ public class Student {
 	private String gender;
 	private Integer rank;
 		
-	
-	public Student() {
-		
-	}
-	
-	public Student(Integer id, String name, String gender, Integer rank) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.gender = gender;
-		this.rank = rank;
-	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getGender() {
-		return gender;
-	}
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-	public Integer getRank() {
-		return rank;
-	}
-	public void setRank(Integer rank) {
-		this.rank = rank;
-	}
-	@Override
-	public String toString() {
-		return "Student [id=" + id + ", name=" + name + ", gender=" + gender + ", rank=" + rank + "]";
-	}
-	
-	
-	
 
 }
