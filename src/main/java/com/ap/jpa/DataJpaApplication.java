c9cbb80ad9f92fbfac9355e5b8dc85382976f266
akshay <akshaypatil781997@gmail.com>
2023-02-13 22:40:08 +0530

    added jpa query

--- src/main/java/com/ap/jpa/DataJpaApplication.java
+++ src/main/java/com/ap/jpa/DataJpaApplication.java
@@ -6,22 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import com.ap.jpa.model.Student;
 import com.ap.jpa.repo.StudentRepository;
 
 @SpringBootApplication
 public class DataJpaApplication {
-	
-	
-	public void m2() {
-		// bug 202 fixed 
-	}
-	
-	public void m1() {
-		//logic of before stash
-		// bug 201 fixed ....
-	}
-	
-	
 
 	public static void main(String[] args) {
 
@@ -29,56 +18,67 @@ public class DataJpaApplication {
 
 		StudentRepository studRepo = context.getBean(StudentRepository.class);
 
-		/*
-		 * List<Student> stud = studRepo.getStudentByGender("male");
-		 * 
-		 * stud.forEach(s->System.out.println(s));
-		 */
+		List<Student> stud = studRepo.getStudentByGender("male");
+
+		stud.forEach(s -> System.out.println(s));
 
 		// List<Student> s = studRepo.getNullValluStudent();
 
 		// List<Student> s = studRepo.getRank(10);
 
-		//List<Student> s = studRepo.getRankLessThan(8);
-		
-		//List<Student> s = studRepo.getByGenderAndRank("female", 8);
-		
-		List<Object[]> s = studRepo.getAllStudents();
-		
-		for(Object[] a:s) {
-			System.out.println(a[0]+" "+a[1]);
-		}
-		
+		// List<Student> s = studRepo.getRankLessThan(8);
 
-		/*
-		 * Student s = new Student();
-		 * 
-		 * s.setId(111); s.setName("akshay"); s.setGender("male"); s.setRank(1);
-		 * 
-		 * studRepo.save(s); System.out.println("Record Inserted .......");
-		 * 
-		 * // fetching all records Iterable<Student> st = studRepo.findAll();
-		 * 
-		 * for (Student ss : st) { System.out.println(ss); }
-		 * 
-		 * st.forEach(System.out::println);
-		 * 
-		 * // fetching single record
-		 * 
-		 * Student student = studRepo.findById(22).get(); System.out.println(student);
-		 * 
-		 * 
-		 * 
-		 * // deleting record
-		 * 
-		 * if (studRepo.existsById(444)) { studRepo.deleteById(444);
-		 * System.out.println("record is deleted");
-		 * 
-		 * } else { System.out.println("record not found"); }
-		 */
-		
-	
-		
+		// List<Student> s = studRepo.getByGenderAndRank("female", 8);
+
+		List<Object[]> s = studRepo.getAllStudents();
+
+		for (Object[] a : s) {
+			System.out.println(a[0] + " " + a[1]);
+		}
+
+		Student s = new Student();
+
+		s.setId(111);
+		s.setName("akshay");
+		s.setGender("male");
+		s.setRank(1);
+
+		studRepo.save(s);
+		System.out.println("Record Inserted .......");
+
+		// fetching all records Iterable<Student> st = studRepo.findAll();
+
+		for (Student ss : st) {
+			System.out.println(ss);
+		}
+
+		st.forEach(System.out::println);
+
+		// fetching single record
+
+		Student student = studRepo.findById(22).get();
+		System.out.println(student);
+
+		// deleting record
+
+		if (studRepo.existsById(444)) {
+			studRepo.deleteById(444);
+			System.out.println("record is deleted");
+
+		} else {
+			System.out.println("record not found");
+		}
+
+		// deleted record using custom query Integer delete =
+		studRepo.deleteStudent(333);
+		System.out.println(delete + " record deleted...");
+
+		// updating record using custom query Integer updatedrecord =
+		studRepo.updateStudent(123, "gita");
+		System.out.println(updatedrecord + " record is updated....");
+
+		studRepo.insertStudent(111, "akshay", "male", 111);
+		System.out.println(" record inserted...");
 
 	}
 
--- src/main/java/com/ap/jpa/repo/StudentRepository.java
+++ src/main/java/com/ap/jpa/repo/StudentRepository.java
@@ -2,7 +2,10 @@ package com.ap.jpa.repo;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -10,36 +13,45 @@ import org.springframework.stereotype.Repository;
 import com.ap.jpa.model.Student;
 
 @Repository
-public interface StudentRepository extends JpaRepository<Student,Integer>{
-	
-	// select * from student_dtls where student_gender=:gender	
+public interface StudentRepository extends JpaRepository<Student, Integer> {
 
-	@Query("from Student where gender=:gender")
-	//@Query(value = "select * from employee where gender='male'",nativeQuery = true)
-	public List<Student> getStudentByGender(String gender);
-	
+	// select * from student_dtls where student_gender=:gender
+	@Query("from Student where gender=:gender") 
+	//@Query(value ="select * from employee where gender='male'",nativeQuery = true) public
+	  List<Student> getStudentByGender(String gender);
+
 	@Query("from Student where gender=null")
 	public List<Student> getNullValluStudent();
-	
+
 	// select * from student_dtls where student_rank >= : rank
-	
 	@Query("from Student where rank>=:rank")
 	public List<Student> getRank(int rank);
-	
-	// select * from student_dtls where student_rank <= : rank
 
+	// select * from student_dtls where student_rank <= : rank
 	@Query("from Student where rank<=:rank")
 	public List<Student> getRankLessThan(int rank);
-	
-	
-	// select * from student_dtls where student_gender=:gender and student_rank >= :rank
-	
-	@Query("from Student where gender=:gender and rank=:rank")
-	public List<Student> getByGenderAndRank(@Param("gender") String gender,@Param("rank") int rank);
 
-	// select  student_rank, student_gender from student_dtls;
+	// select * from student_dtls where student_gender=:gender and student_rank>=:rank
+	@Query("from Student where gender=:gender and rank=:rank")
+	public List<Student> getByGenderAndRank(@Param("gender") String gender, @Param("rank") int rank);
+
+	// select student_rank, student_gender from student_dtls;
 	@Query("select S.gender,S.rank from Student S")
 	public List<Object[]> getAllStudents();
-	
+
+	@Modifying
+	@Transactional
+	@Query("delete from Student where id=:id")
+	public Integer deleteStudent(int id);
+
+	@Modifying
+	@Transactional
+	@Query("update Student set name=:name where id=:id")
+	public Integer updateStudent(int id, String name);
+
+	@Query(value = "insert into student_details(id,name,gender,rank)values(:id,:name,:gender,:rank);", nativeQuery = true)
+	@Modifying
+	@Transactional
+	public void insertStudent(Integer id, String name, String gender, Integer rank);
 
 }
